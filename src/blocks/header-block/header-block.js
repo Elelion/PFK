8672c2a4faf45c6aa95b17d3bfe458b8cefef480
unknown <elelion@yandex.ru>
2019-05-14 23:06:27 +0300

    Дописали в класс новые DOM елементы для стилизации

--- src/blocks/header-block/header-block.js
+++ src/blocks/header-block/header-block.js
@@ -8,6 +8,8 @@ class headerBlock {
 
   initDOMElements() {
     this.menu = document.getElementsByClassName('header-block')[0];
+    this.logo = document.getElementsByClassName('header-block__logo')[0];
+    this.wrapper = document.getElementsByClassName('header-block__wrapper')[0];
   }
 
   // **
@@ -17,12 +19,37 @@ class headerBlock {
       event.preventDefault();
 
       this.scroll = window.pageYOffset || document.documentElement.scrollTop;
-      this.getShowMenu(this.scroll > 100 ? 'fixed' : 'absolute');
+
+      if (this.scroll > 100) {
+        this.getShowMenu('fixed');
+        this.getHeightMenu('4.1rem');
+        this.getDisplayLogo('none');
+        this.getFlex('block');
+      } else {
+        this.getShowMenu('absolute');
+        this.getHeightMenu('8rem');
+        this.getDisplayLogo('block');
+        this.getFlex('flex');
+      }
     })
   }
 
+  // **
+
   getShowMenu(position) {
     this.menu.style.position = position;
+  }
+
+  getHeightMenu(height) {
+    this.menu.style.height = height;
+  }
+
+  getDisplayLogo(display) {
+    this.logo.style.display = display;
+  }
+
+  getFlex(value) {
+    this.wrapper.style.display = value;
   }
 
   // **
