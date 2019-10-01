363354ff6f22fb101ad4f3697b91b8d4233659a1
unknown <elelion@yandex.ru>
2019-10-01 13:19:59 +0300

    Оптимизация кода

--- debug/index.php
+++ debug/index.php
@@ -2,47 +2,19 @@
 require_once 'lib/functions.php';
 date_default_timezone_set('Europe/Moscow');
 new RedirectNotSupportBrowser();
+$dbHelper = new DbHelper;
 
 // **
 
-$link = connectDB();
-$sqlQuery = 'SELECT * FROM `articles` ORDER BY id DESC LIMIT 6';
-$getResultData = requestSQL($link, $sqlQuery);
-
-foreach ($getResultData as $row) {
-  $titleDB = $row['title'];
-  $miniDesc = $row['miniDescription'];
-  $imageDB = $row['image_file'];
-  $idDB = $row['id'];
+if (!$dbHelper->getLastError()) {
+  $dbHelper->executeQuery('SELECT * FROM `articles` ORDER BY id DESC LIMIT 6');
 }
 
-$sqlQuery = 'SELECT * FROM articles ORDER BY id DESC LIMIT 2';
-$countRow = requestSQL($link, $sqlQuery, 'count');
-// echo "$countRow Rows\n";
+$queryResult = $dbHelper->getQueryResult();
 
-// $link = connectDB();
-
-// $sql = "SELECT * FROM category";
-// $result = mysqli_query($link, $sql);
-
-// NOTE: MYSQLI_ASSOC - возвращает массив в читабельном виде (типа матрици)
-// $category_list = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-// $sql_lot = "SELECT
-//   title AS name,
-//   start_price as price,
-//   image_file as url,
-//   lot.id, #добавляем для id по нему будет происходить линк на др. стр
-//   category.name as category
-//   FROM lot
-//   INNER JOIN category ON lot.category_id = category.id";
-
-// $result_lot = mysqli_query($link, $sql_lot);
-// $product_list = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
-
-// $test = new connectDBase('test', 'test', 'test', 'test');
+// **
 
 $file = basename(__FILE__, ".php");
 require './src/' . $file . '.html';
 
-//НЕ ЗАКРЫВАТЬ PHP!!!...
+// TODO: don't close!
