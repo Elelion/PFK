42dd798d6941df0c4b0aff1825412839c949e5e9
unknown <elelion@yandex.ru>
2019-10-01 13:18:38 +0300

    Оптимизация кода

--- debug/browsing-articles.php
+++ debug/browsing-articles.php
@@ -1,24 +1,22 @@
 <?php
 require_once 'lib/functions.php';
 date_default_timezone_set('Europe/Moscow');
+new RedirectNotSupportBrowser();
+$dbHelper = new DbHelper;
 
 // **
 
-$link = connectDB();
-
-// **
-
-// NOTE: services list
-if (empty($_GET['idArticles'])) {
-  Error404();
+if ((empty($_GET['idArticles'])) || (!is_numeric($_GET['idArticles']))) {
+  header('Location: ./404.php');
 } else {
-  $id = mysqli_real_escape_string($link, $_GET['idArticles']);
-  $sqlQuery = "SELECT * FROM articles WHERE id = '$id'";
+  if (!$dbHelper->getLastError()) {
+    $id = mysqli_real_escape_string($dbHelper->getConnect(), $_GET['idArticles']);
+    $dbHelper->executeQuery("SELECT * FROM articles WHERE id = '$id'");
+  }
 
-  $result = mysqli_query($link, $sqlQuery);
-  $resultList = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $queryResult = $dbHelper->getQueryResult();
 
-  foreach ($resultList as $row) {
+  foreach ($queryResult as $row) {
     $title = $row['title'];
     $time = $row['time_create'];
     $date = $row['date_create'];
@@ -31,4 +29,4 @@ if (empty($_GET['idArticles'])) {
 $file = basename(__FILE__, '.php');
 require './src/' . $file . '.html';
 
-// NOTE: don't close!
+// TODO: don't close!
