499452370ecf95926d2d3a5b07cb522629417aec
unknown <elelion@yandex.ru>
2019-10-08 15:22:51 +0300

    Разбили каждый класс по файлу

--- debug/src/js/FeedBackCheck.ts
+++ debug/src/js/FeedBackCheck.ts
@@ -1,38 +1,5 @@
 /*jshint esversion: 6 */
 
-// NOTE: index -> all articles button (see bottom of page)
-class ButtonsFollowing {
-	private buttonName: string;
-	private followingLink: string;
-	private buttons: any;
-
-	constructor(name: string = '', link: string = '#') {
-		this.buttonName = name;
-		this.followingLink = link;
-
-		this.buttons = document.querySelectorAll('.' + this.buttonName);
-
-		this.beginEvent();
-	}
-
-	setFollowingLink(link: string = '#') {
-		document.location.href = link;
-	}
-
-	beginEvent() {
-		for (let i = 0; i < this.buttons.length; i += 1) {
-			this.buttons[i].addEventListener('click', (event: any) => {
-				this.setFollowingLink(this.followingLink);
-				event.preventDefault();
-			});
-		}
-	}
-}
-
-new ButtonsFollowing('button-standard__extension-articles', 'all-articles.php');
-
-// **
-
 class ButtonsContactFeedBackCheck {
 	private name: any;
 	private phone: any;
@@ -130,7 +97,7 @@ class ButtonsContactFeedBackCheck {
 
 new ButtonsContactFeedBackCheck('button-standard__extension-contacts');
 
-// **
+// ----------------------------------------------------------------------------
 
 class ButtonsServiceFeedBackCheck extends ButtonsContactFeedBackCheck {
 	private address: any;
